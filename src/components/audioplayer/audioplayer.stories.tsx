04c0b47fb6d33e9d059a37c0d533ac7df3855c2b
mrideout1985 <mrideout1985@gmail.com>
2022-06-16 22:58:29 +0100

    storybook stuff.

--- src/components/audioplayer/audioplayer.stories.tsx
+++ src/components/audioplayer/audioplayer.stories.tsx
@@ -5,12 +5,31 @@ import AudioPlayer from './audioplayer'
 const meta: Meta = {
     title: 'Audio Player',
     component: AudioPlayer,
+    argTypes: {
+        color: [
+            'inherit',
+            'default',
+            'primary',
+            'secondary',
+            'error',
+            'info',
+            'success',
+            'warning',
+        ],
+    },
 }
 
 export default meta
 
-const Template: Story<typeof AudioPlayer> = () => (
-    <AudioPlayer src="source" transcription="dicks" />
+const Template: Story<typeof AudioPlayer> = ({ color }: any) => (
+    <AudioPlayer
+        src="http://www.hyperion-records.co.uk/audiotest/18%20MacCunn%20The%20Lay%20of%20the%20Last%20Minstrel%20-%20Part%202%20Final%20chorus%20O%20Caledonia!%20stern%20and%20wild.MP3"
+        color={color}
+    />
 )
 
-export const Default = Template.bind({})
+export const Player = Template.bind({})
+Player.args = {
+    color: 'primary',
+    src: 'source',
+}
