878a0200a1c1674fe519f78049502deb9116dae7
mrideout1985 <mrideout1985@gmail.com>
2022-06-16 22:59:01 +0100

    created mute function & functionality to allow the volume slider to mirror this.

--- src/components/audioplayer/audioplayer.tsx
+++ src/components/audioplayer/audioplayer.tsx
@@ -1,17 +1,7 @@
-import {
-    Box,
-    Button,
-    Container,
-    Grow,
-    IconButton,
-    Menu,
-    MenuItem,
-    Slider,
-    Stack,
-    Typography,
-} from '@mui/material'
+import { IconButton, Menu, MenuItem, Slider, Typography } from '@mui/material'
 import React, { MutableRefObject, useEffect, useState, useRef } from 'react'
 import VolumeUpIcon from '@mui/icons-material/VolumeUp'
+import VolumeOffIcon from '@mui/icons-material/VolumeOff'
 import SettingsIcon from '@mui/icons-material/Settings'
 import { PlayPauseButton } from './playPauseButton/playPauseButton'
 import styles from './audioplayer.module.scss'
@@ -25,14 +15,14 @@ import { Skip } from './skip/skip'
 
 interface AudioPlayer {
     src: any
-    transcription: string
+    color: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning'
 }
 
-function AudioPlayer({ src, transcription }: AudioPlayer) {
+function AudioPlayer({ src, color }: AudioPlayer) {
     const [playing, setPlaying] = useState(false)
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
     const [mediaTime, setMediaTime] = useState<number>(0)
-    const [volume, setVolume] = useState<number>(1)
+    const [volume, setVolume] = useState<number>(0.5)
     const [duration, setDuration] = useState<number>(0)
     const ref = useRef() as MutableRefObject<HTMLAudioElement>
     const open = Boolean(anchorEl)
@@ -46,7 +36,7 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
         onLoadedMetaData()
     }, [playing])
 
-    const togglePLaying = (event: React.SyntheticEvent) => {
+    const togglePLaying = (event: any) => {
         setPlaying(!playing)
     }
 
@@ -65,8 +55,19 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
     }
     const volumeChange = (e: any) => {
         const parsedVolume = parseInt(e.target.value) / 100
-        setVolume(parsedVolume)
         ref.current.volume = parsedVolume
+        setVolume(ref.current.volume)
+        ref.current.muted = false
+    }
+
+    const handleMute = () => {
+        if (ref.current.muted) {
+            ref.current.muted = false
+            setVolume(ref.current.volume)
+        } else {
+            ref.current.muted = true
+            setVolume(0)
+        }
     }
 
     const changeSpeed = (rate: number) => {
@@ -87,37 +88,46 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
         ref.current.currentTime = newTime
     }
 
-    const displaySongCounter = () => {
-        return duration >= 18000
-            ? new Date(mediaTime * 1000).toISOString().substring(11, 19)
-            : new Date(mediaTime * 1000).toISOString().substring(14, 19)
-    }
     return (
         <div className={styles.container}>
             <div className={styles.audio}>
                 <div className={styles.controls}>
-                    <Rewind rewind={backFive} />
+                    <Rewind rewind={backFive} color={color} />
                     <PlayPauseButton
                         playing={playing}
                         togglePlaying={togglePLaying}
+                        color={color}
                     />
-                    <Skip skip={forwardFive} />
+                    <Skip skip={forwardFive} color={color} />
                 </div>
                 <div className={styles.playbackContainer}>
                     <div className={styles.volume}>
-                        <VolumeUpIcon color="primary" tabIndex={0} />
+                        <IconButton color={color} onClick={() => handleMute()}>
+                            {ref.current?.muted ? (
+                                <VolumeOffIcon />
+                            ) : (
+                                <VolumeUpIcon />
+                            )}
+                        </IconButton>
                         <Slider
                             aria-label="Volume"
                             min={0}
                             max={100}
                             size="small"
-                            defaultValue={30}
-                            onChange={(e) => volumeChange(e)}
+                            value={volume * 100}
+                            color={color as 'primary' | 'secondary' | undefined}
+                            onChange={(e: any) => volumeChange(e)}
                             className={styles.volumeSlider}
                         />
                     </div>
                     <Typography fontWeight={'100'}>
-                        {displaySongCounter()}
+                        {duration >= 18000
+                            ? new Date(mediaTime * 1000)
+                                  .toISOString()
+                                  .substring(11, 19)
+                            : new Date(mediaTime * 1000)
+                                  .toISOString()
+                                  .substring(14, 19)}
                     </Typography>
                     <Slider
                         value={mediaTime ?? 0}
@@ -125,6 +135,7 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
                         max={duration || 100}
                         onChange={scrubberChange}
                         sx={{ margin: '0 1rem', width: '100%' }}
+                        color={color as 'primary' | 'secondary' | undefined}
                     />
                     <Typography fontWeight={'100'}>
                         {duration
@@ -136,12 +147,12 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
                     <div className={styles.settings}>
                         <IconButton
                             aria-label="settings"
-                            color={'primary'}
                             size="small"
                             id="basic-button"
                             aria-controls={open ? 'basic-menu' : undefined}
                             aria-haspopup="true"
                             aria-expanded={open ? 'true' : undefined}
+                            color={color}
                             onClick={(
                                 event: React.MouseEvent<HTMLButtonElement>
                             ) => setAnchorEl(event.currentTarget)}
@@ -164,7 +175,7 @@ function AudioPlayer({ src, transcription }: AudioPlayer) {
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                {[1, 1.25, 1.5, 1.75, 2].map((rate) => (
+                {[0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2].map((rate) => (
                     <MenuItem
                         key={rate}
                         onClick={() => changeSpeed(rate)}
